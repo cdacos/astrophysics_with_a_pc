0cf6061d36d3f101fea5f879814c1d2e43a1eb03
Carlos da Costa <carlos@73bit.com>
2014-02-08 21:43:32 +0000

    Precision?

--- ch03_meteor_dynamics.py
+++ ch03_meteor_dynamics.py
@@ -12,14 +12,14 @@ by Carlos da Costa https://github.com/cdacos (2014-02-07)
 
 from __future__ import print_function, division
 from helpers import start_parameter
-from math import fabs, exp, sqrt, log
+from math import fabs, exp, sqrt, log, pow
 
 def datm(y):
   """Computes the atmospheric density at height y(cm)
   """
   return exp(-6.65125 - 1.39813e-6 * y)
 
-def effes(x, y, u, v, m, k1, k2, tau):
+def effes(u, v, m, k1, k2, tau):
   """Computes fx, fy, fu, fv, fm and s (the speed) for a position (x,y)
   velocity (u,v) and mass (m), and with parameters k1,k2 and tau
   """
@@ -27,10 +27,9 @@ def effes(x, y, u, v, m, k1, k2, tau):
   fy = v
   s = sqrt(u**2 + v**2)
   rho = datm(y)
-  print('u={:f}, v={:f}, m={:f}, s={:f}, rho={:f}'.format(u, v, m, s, rho))
-  fu = -k1 * rho * s * u * exp(-1/3 * log(m))
-  fv = -k1 * rho * s * v * exp(-1/3 * log(m)) - 980
-  fm = -k2 * rho * s**3 * exp(2/3 * log(m))
+  fu = -k1 * rho * s * u * pow(m, -1.0/3)
+  fv = -k1 * rho * s * v * pow(m, -1.0/3) - 980
+  fm = -k2 * rho * s*s*s * pow(m, 2.0/3)
   return [fx, fy, fu, fv, fm, s]
 
 print('Astrophysics with a PC : METEOR')
@@ -49,18 +48,18 @@ tau = start_parameter('Parameter tau : ', 7)
 
 # transform input data x, y, u and v from km to cm
 # and make sure that y is negative
-y = y + 100000
+y = y * 100000
 x = .0
 u = u * 100000
 v = -1 * fabs(v * 100000)
 minit = m
 t = .0
-i = 1.0
+i = 1
 print('')
-print('   i   t    x    y    u    v    m    mag')
+print(' i    t      x        y        u        v        m        mag')
 print (' ')
 
-while m >= minit: # This is the main loop of the program
+while m >= minit * .01: # This is the main loop of the program
 
   # next 3 nested if-blocks select the time the time step dt
   if m > .8 * minit:
@@ -75,7 +74,7 @@ while m >= minit: # This is the main loop of the program
   t = t + dt
 
   # next line computes right hand sides of the 5 differential equations (state i)
-  fx, fy, fu, fv, fm, s = effes(x, y, u, v, m, k1, k2, tau)
+  fx, fy, fu, fv, fm, s = effes(u, v, m, k1, k2, tau)
 
   # next 5 instructions compute the predicted state 'i+1
   x1 = x + dt * fx
@@ -85,7 +84,7 @@ while m >= minit: # This is the main loop of the program
   m1 = m + dt * fm
 
   # next line computes rigt hand sides of the 5 differential equations (state 'i+1)
-  fx1, fy1, fu1, fv1, fm1, s1 = effes(x1, y1, u1, v1, m1, k1, k2, tau)
+  fx1, fy1, fu1, fv1, fm1, s1 = effes(u1, v1, m1, k1, k2, tau)
 
   # next 5 instructions compute the correct state at 'i+1
   x = x + .5 * dt * (fx + fx1)
@@ -95,25 +94,24 @@ while m >= minit: # This is the main loop of the program
   m = m + .5 * dt * (fm + fm1)
 
   # Computation of the apparent magnitude :
-  print('fm1={:f}, s1={:f}'.format(fm1, s1))
   e = -.5 * tau * fm1 * s1**2
-  mag = 5 * log(y, 10) - 2.5 * log(e, 10) - 8.795
+  mag = 5. * log(y, 10) - 2.5 * log(e, 10) - 8.795
 
   # Results are shwon on screen :
   if y > 0:
-    print('{: 2d}/', i, t, x / 100000, y / 100000, u / 100000, v / 100000, m, mag)
+    print('{:2d} {:5.2f} {:8.4f} {:8.4f} {:9.5f} {:9.5f} {:9.7f} {:5.2f}'.format(i, t, x / 100000, y / 100000, u / 100000, v / 100000, m, mag))
   else:
     print('')
     print('Meteoroid has reached the ground')
     m = 0
 
-  i = i + 1
-
-  if i % 15 == 0:
+  if i % 10 == 0:
     # this if makkes the program pause after every 15 iterations
     print('')
     raw_input('press enter to continue')
     print('')
+
+  i = i + 1
 
 print('')
 print('Program terminated.')
